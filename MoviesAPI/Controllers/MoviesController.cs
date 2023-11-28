8a39727ca72552ba3ad290dae305a446dbea58f0
AnMal <junkie_@msn.com>
2023-11-28 12:05:03 +0100

    Add project files.

---
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Services;
+using MoviesAPI.ViewModels;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MoviesService _moviesService;
+
+        public MoviesController(MoviesService moviesService)
+        {
+            _moviesService = moviesService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllMovies()
+        {
+            var movies = _moviesService.GetAllMovies();
+            return Ok(movies);
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetMovieById([FromQuery] int id)
+        {
+            var movie =  _moviesService.GetMovieById(id);
+            return Ok(movie);
+        }
+
+        [HttpPut("id")]
+        public IActionResult UpdateMovie([FromQuery] int id, [FromBody]MovieVM movie)
+        {
+            var updatedMovie = _moviesService.UpdateMovie(id, movie);
+            return Ok(updatedMovie);
+        }
+
+        [HttpDelete("id")]
+        public IActionResult DeleteMovie(int id)
+        {
+            _moviesService?.DeleteMovie(id);
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public IActionResult AddMovie(MovieVM movie)
+        {
+            _moviesService.AddMovie(movie);
+            return Ok();
+        }
+        
+
+
+    }
+}
---
+++ MoviesAPI/Services/MoviesService.cs
@@ -0,0 +1,66 @@
+﻿using MoviesAPI.Data;
+using MoviesAPI.ViewModels;
+
+namespace MoviesAPI.Services
+{
+    public class MoviesService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public MoviesService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return _dbContext.Movies.ToList();
+        }
+
+        public Movie? GetMovieById(int id)
+        {
+            return _dbContext.Movies.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Movie? UpdateMovie(int id, MovieVM movieVM) 
+        {
+            Movie? movie = _dbContext.Movies.FirstOrDefault(x => x.Id == id);
+
+            if (movie is not null)
+            {
+                movie.Name = movieVM.Name;
+                movie.Year = movieVM.Year;
+                movie.Genre = movieVM.Genre;
+
+                _dbContext.Movies.Update(movie);
+                _dbContext.SaveChanges();
+            }
+
+            return movie;
+        }
+
+        public void DeleteMovie(int id)
+        {
+            var movie = _dbContext.Movies.FirstOrDefault(x => x.Id == id);
+
+            if (movie is null) 
+                return;
+
+            _dbContext.Movies.Remove(movie);
+            _dbContext.SaveChanges();
+        }
+
+        public void AddMovie(MovieVM movieVM)
+        {
+            var newMovie = new Movie()
+            {
+                Name = movieVM.Name,
+                Year = movieVM.Year,
+                Genre = movieVM.Genre
+            };
+
+            _dbContext.Movies.Add(newMovie);
+            _dbContext.SaveChanges();
+        }
+    }
+}
